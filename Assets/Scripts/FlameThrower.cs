e4223e7fd810f6f675fc6c27c7b55f844671c4bf
Thibaut Piquerez <thib.piquerez@gmail.com>
2019-05-08 08:59:30 +0200

    fire network

--- Assets/Scripts/FlameThrower.cs
+++ Assets/Scripts/FlameThrower.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class FlameThrower : MonoBehaviour
+public class FlameThrower : NetworkBehaviour
 {
 
     [SerializeField]
@@ -30,6 +31,9 @@ public class FlameThrower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!isLocalPlayer)
+            return;
+
         if (Input.GetKeyDown(KeyCode.F))
         {
             GameObject fire = Instantiate(firePrefabs);
